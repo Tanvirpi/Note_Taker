f63dac095ee802a665d67c4de01e6f28e0f969cb
tanvirpi <tanvirpi@gmail.com>
2023-04-16 19:14:14 -0600

    Add PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const api = require('./routes/apiRoutes');
 const htmlROute = require('./routes/htmlRoutes');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
